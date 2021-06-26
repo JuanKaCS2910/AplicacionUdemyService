8966d39228f1b2d4645e5ab6b477ecfc6e2e6b87
Juan Carlos Castro Socla <juan.castro.socla@gmail.com>
2021-06-25 23:17:20 -0500

    Add project files.

---
+++ AplicacionUdemyService.Datos/PaisesDTO.cs
@@ -0,0 +1,51 @@
+﻿using AplicacionUdemyService.Entity.Parametros;
+using AplicacionUdemyService.Entity.Response;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionUdemyService.Datos
+{
+    public class PaisesDTO
+    {
+        public List<ResponsePais> listarPaises(RegistroEmpresaEntity paramss)
+        {
+			try
+			{
+				string cs = ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString;
+				var lista = new List<ResponsePais>();
+
+				using (SqlConnection conn = new SqlConnection(cs))
+				{
+					conn.Open();
+					SqlCommand cmd = new SqlCommand("Usp_listarPaises",conn);
+					cmd.CommandType = CommandType.StoredProcedure;
+
+					using (SqlDataReader dr = cmd.ExecuteReader())
+					{
+						while (dr.Read())
+						{
+							var _result = new ResponsePais();
+							_result.idPais = Convert.ToInt32(dr["idPais"]);
+							_result.nombre = Convert.ToString(dr["pais"]);
+							lista.Add(_result);
+						}
+					}
+				}
+				return lista;
+			}
+			catch (Exception ex)
+			{
+
+				throw ex;
+			}
+
+			
+		}
+    }
+}
---
+++ AplicacionUdemyService/Controllers/RegistroEmpresaController.cs
@@ -0,0 +1,39 @@
+﻿using AplicacionUdemyService.Datos;
+using AplicacionUdemyService.Entity.Parametros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AplicacionUdemyService.Controllers
+{
+    [RoutePrefix("api/RegistroEmpresa")]
+    public class RegistroEmpresaController : ApiController
+    {
+        private PaisesDTO paisesDTO;
+
+        public RegistroEmpresaController()
+        {
+            paisesDTO = new PaisesDTO();
+        }
+
+        [HttpPost]
+        [Route("listarPaises")]
+        public IHttpActionResult listarPaises(RegistroEmpresaEntity paramss)
+        {
+            try
+            {
+                var _result = paisesDTO.listarPaises(paramss);
+                return Ok(_result);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+    }
+}
